28faabeb5cea2da5393743d74b3a1950358e930d
Ksl-slasher <koslowskileonardo84@gmail.com>
2024-03-15 14:35:10 -0300

    função adicionarContato criada

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
+require 'config.php';
+require 'src/Agenda.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' ) {
+
+    $contato = new Agenda($mysql);
+    $contato->adicionarContato($_POST['nome'], $_POST['endereco'], $_POST['cidade'], $_POST['estado'], $_POST['email'], $_POST['telefone']);
+
+
+}
+
 
 ?>
 <!DOCTYPE html>
@@ -14,41 +25,38 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
-    
+
 
 </head>
 
 <body>
     <header class="formulario-entrada">
         <h1>Agenda</h1>
-        <form>
+        <form action="index.php" method="POST">
 
             <label for="Nome">Nome</label>
-            <input type="text" placeholder="John Doe" required>
+            <input name="nome" type="text" placeholder="John Doe" id='nome' required>
 
             <label for="Endereço">Endereço</label>
-            <input type="text" placeholder="Rua Capital, 33" required>
+            <input name="endereco" type="text" placeholder="Rua Capital, 33" id="endereco" required>
 
             <label for="Cidade">Cidade</label>
-            <input type="text" placeholder="São Bento do Sul" required>
+            <input name="cidade" type="text" placeholder="São Bento do Sul" id="cidade" required>
 
             <label for="Estado">Estado</label>
-            <input type="text" placeholder="Santa Catarina" required>
+            <input name="estado" type="text" placeholder="Santa Catarina" id="estado" required>
 
             <label for="Email">Email</label>
-            <input type="email" placeholder="JohnDoe@exemplo.com" required>
+            <input name="email" type="email" placeholder="JohnDoe@exemplo.com" id="email" required>
 
             <label for="Telefone">Telefone</label>
-            <input type="text" placeholder="(XX)XXXXX-XXXX" required>
+            <input name="telefone" type="text" placeholder="(XX)XXXXX-XXXX" id="telefone" required>
 
             <input id="salvar" type="submit" value="Salvar contato">
 
         </form>
 
     </header>
-    <main>
-
-    </main>
 
 </body>
 
--- src/Agenda.php
+++ src/Agenda.php
@@ -1,1 +1,24 @@
 <?php
+
+class Agenda
+{
+    
+
+    private $mysql;
+
+
+    public function __construct(mysqli $mysql)
+    {
+        $this->mysql = $mysql;
+    }
+    // Create
+    public function adicionarContato(string $nome, string $endereco, string $cidade, string $estado, string $email, string $telefone): void
+    {
+        $adicionaContato = $this->mysql->prepare('INSERT INTO `contatos` (`nome`, `endereco`, `cidade`, `estado`, `email`, `telefone` ) VALUES (?, ?, ?, ?, ?, ?) ;');
+        $adicionaContato->bind_param('ssssss', $nome, $endereco, $cidade, $estado, $email, $telefone);
+        $adicionaContato->execute();
+
+    }
+    
+
+}
