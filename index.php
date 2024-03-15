91af904fcc875d4ab3b1d2a479f8ea822f73b032
Ksl-slasher <koslowskileonardo84@gmail.com>
2024-03-15 13:26:31 -0300

    Layout do formulário HTML criado

--- index.php
+++ index.php
@@ -1,14 +1,55 @@
 <?php
 
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+    <title>Agenda</title>
+
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
     
+
+</head>
+
+<body>
+    <header class="formulario-entrada">
+        <h1>Agenda</h1>
+        <form>
+
+            <label for="Nome">Nome</label>
+            <input type="text" placeholder="John Doe" required>
+
+            <label for="Endereço">Endereço</label>
+            <input type="text" placeholder="Rua Capital, 33" required>
+
+            <label for="Cidade">Cidade</label>
+            <input type="text" placeholder="São Bento do Sul" required>
+
+            <label for="Estado">Estado</label>
+            <input type="text" placeholder="Santa Catarina" required>
+
+            <label for="Email">Email</label>
+            <input type="email" placeholder="JohnDoe@exemplo.com" required>
+
+            <label for="Telefone">Telefone</label>
+            <input type="text" placeholder="(XX)XXXXX-XXXX" required>
+
+            <input id="salvar" type="submit" value="Salvar contato">
+
+        </form>
+
+    </header>
+    <main>
+
+    </main>
+
 </body>
+
 </html>
